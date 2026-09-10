4956ed4b05c88a90d7e2a8f0b98162754f5324c3
vensah-dev <venkateshdeven14@gmail.com>
2026-09-10 12:06:00 +0800

    cewvwevwec

--- src/components/shared/WebcamFeed.jsx
+++ src/components/shared/WebcamFeed.jsx
@@ -5,19 +5,20 @@ const viewHeight = window.innerHeight
 
 export default function WebcamFeed({ videoRef, canvasRef, cameraRotation }) {
   return (
-<div className="relative h-full w-full">
+<div className="relative h-full w-full " style={{ transform: `rotate(${cameraRotation || 0}deg)` }}>
       <video
         ref={videoRef}
         autoPlay
         playsInline
         fill
-        style={{ transform: `rotate(-${cameraRotation || 0}deg)` }}
+        
         className={`absolute inset-0 w-screen h-screen aspect[${viewWidth}/${viewHeight}] -scale-x-100 object-cover`}
       />
       <canvas
         ref={canvasRef}
         width={viewWidth}
         height={viewHeight}
+        style={{ transform: `rotate(${cameraRotation || 0}deg)` }}
         className="pointer-events-none absolute inset-0 z-10 h-full w-full -scale-x-100 rounded-lg "
       />
     </div>
